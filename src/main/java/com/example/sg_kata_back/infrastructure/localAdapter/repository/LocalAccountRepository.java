540dd70721deb9cb714b7ae645473446c2da5ad4
cedric duperron <cedric.duperron@gmail.com>
2025-07-01 06:25:04 +0200

    update local account repository

--- src/main/java/com/example/sg_kata_back/infrastructure/localAdapter/repository/LocalAccountRepository.java
+++ src/main/java/com/example/sg_kata_back/infrastructure/localAdapter/repository/LocalAccountRepository.java
@@ -2,6 +2,7 @@ package com.example.sg_kata_back.infrastructure.localAdapter.repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -13,24 +14,33 @@ import com.example.sg_kata_back.domain.account.AccountTransaction;
 @Component
 public class LocalAccountRepository {
 
-    public List<AccountTransaction> transactions = List.of(
+    public List<AccountTransaction> transactions = new ArrayList<>(List.of(
             AccountTransaction.builder().type("deposit")
-                    .amount(new BigDecimal(100)).dateTime(LocalDateTime.now()).id("id1").build());
+                    .amount(new BigDecimal(100)).dateTime(LocalDateTime.now()).id("id1").build()));
 
     public AccountStatement accountStatement = AccountStatement.builder().status("positive").transactions(transactions)
             .build();
 
-    public AccountBalance accountBalance = AccountBalance.builder().currency("EUR").amount(new BigDecimal(100)).build();
+    public AccountBalance accountBalance = AccountBalance.builder().currency("EUR")
+            .amount(computeBalanceAmount(transactions)).build();
 
     public AccountBalance deposit(BigDecimal amount) {
-        BigDecimal updatedAmount = accountBalance.amount().add(amount);
-        accountBalance.withAmount(updatedAmount);
-        return accountBalance;
+        this.transactions.add(AccountTransaction.builder().type("deposit").amount(amount).dateTime(LocalDateTime.now())
+                .id("newId").build());
+        return AccountBalance.builder().currency("EUR").amount(computeBalanceAmount(transactions)).build();
     }
 
     public AccountBalance withdraw(BigDecimal amount) {
-        BigDecimal updatedAmount = accountBalance.amount().subtract(amount);
-        accountBalance.withAmount(updatedAmount);
-        return accountBalance;
+        this.transactions.add(AccountTransaction.builder().type("withdraw").amount(amount).dateTime(LocalDateTime.now())
+                .id("newId").build());
+        return AccountBalance.builder().currency("EUR").amount(computeBalanceAmount(transactions)).build();
+    }
+
+    private BigDecimal computeBalanceAmount(List<AccountTransaction> transactions) {
+        BigDecimal totalDeposit = transactions.stream().filter(c -> c.type().equals("deposit")).map(c -> c.amount())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal totalWithDraw = transactions.stream().filter(c -> c.type().equals("withdraw")).map(c -> c.amount())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return totalDeposit.subtract(totalWithDraw);
     }
 }
